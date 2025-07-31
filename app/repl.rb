021014787428297210a5f443e5430c4f121d942c
Vinny Diehl <vinny.diehl@gmail.com>
2025-07-31 08:40:56 -0700

    Add .gitignore
    Removed `errors/` and `repl.rb` from tracking.

--- app/repl.rb
+++
@@ -1,7 +0,0 @@
-# Remove the x from xrepl to run the code. Add the x back to ignore it.
-# This code in the repl blocks are run in your game console once,
-# when this file is saved.
-
-xrepl do
-  puts "Hello world!"
-end
