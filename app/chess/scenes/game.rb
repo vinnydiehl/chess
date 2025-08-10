73602251582bf95370f731d0017f3ad7db9fad41
Vinny Diehl <vinny.diehl@gmail.com>
2025-08-10 00:45:25 -0700

    Remove `@en_passant_target` preset
    This is set by the FEN, so it's unneccesary.

--- app/chess/scenes/game.rb
+++ app/chess/scenes/game.rb
@@ -5,7 +5,6 @@ class ChessGame
     @square_size = @board_size / 8
 
     @board = Array.new(8) { Array.new(8, nil) }
-    @en_passant_target = nil
 
     @piece_held = nil
     @piece_original_pos = nil
