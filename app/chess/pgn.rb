cd296749cbab557820bb5acbf61a3f0dfd582194
Vinny Diehl <vinny.diehl@gmail.com>
2025-08-25 01:51:39 -0700

    Tokenize string escapes properly

--- app/chess/pgn.rb
+++ app/chess/pgn.rb
@@ -3,17 +3,17 @@
 
 # Test PGN
 PGN = <<EOS
-[Event "F/S Return Match"]
-[Site "Belgrade, Serbia JUG"]
+[Event "F\\\\S Return Match"]
+[Site "Belgrade, \\"Serbia\\" JUG"]
 [Date "1992.11.04"]
 [Round "29"]
 [White "Fischer, Robert J.":"P2"]
 [Black "Spassky, Boris V."]
 [Result "1/2-1/2"]
 
-1.e4 e5 2.Nf3 Nc6 3.Bb5 {This opening is called the Ruy Lopez.} 3...a6
+1.e4 e5 2.Nf3 Nc6 3.Bb5 {This opening is called the "Ruy Lopez".} 3...a6
 4.Ba4 Nf6 5.O-O Be7 6.Re1 b5 7.Bb3 d6 8.c3 O-O 9.h3 Nb8 10.d4 Nbd7
-11.c4 c6 12.cxb5 axb5 13.Nc3 Bb7 14.Bg5 b4 ; Line comment test
+11.c4 c6 12.cxb5 axb5 13.Nc3 Bb7 14.Bg5 b4 ; Line comment "test"!!!
 15.Nb1 h6 16.Bh4 c5 17.dxe5 $242
 Nxe4 18.Bxe7 Qxe7 19.exd6 Qf6 20.Nbd2 Nxd6 21.Nc4 Nxc4 22.Bxc4 Nb6
 23.Ne5 Rae8 24.Bxf7+ Rxf7 25.Nxf7 Rxe1+ 26.Qxe1 Kxf7 27.Qe3 Qg5 28.Qxg5
@@ -41,22 +41,16 @@ class ChessGame
       if build
         case build
         when :string
-          if c == "\\"
-            # Set escape mode if we need to
-            if !escaping
-              escaping = true
-              next
-            end
-
-            # Otherwise we're just escaping a \
+          if escaping
             current << c
             escaping = false
+          elsif c == "\\"
+            escaping = true
           elsif c == '"'
             # End the token at the terminating "
             build = nil
             tokens << current
             current = ""
-            next
           else
             current << c
           end
