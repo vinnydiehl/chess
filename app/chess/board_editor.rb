a9a0b5a45e4fbd3c5a6a159550bb5012d0244b07
Vinny Diehl <vinny.diehl@gmail.com>
2025-09-19 00:10:58 -0700

    Fix crash when clicking on left side of screen
    It was always thinking the board editor was there.

--- app/chess/board_editor.rb
+++ app/chess/board_editor.rb
@@ -2,7 +2,7 @@ class ChessGame
   # Returns a Piece according to the mouse's position on the
   # board editor, or nil if the mouse isn't over the editor.
   def board_editor_mouse_piece
-    return unless @mouse.intersect_rect?(@board_editor_rect)
+    return unless @editing_board && @mouse.intersect_rect?(@board_editor_rect)
 
     # Relative x and y values
     x = @mouse.x - @board_editor_rect.x
