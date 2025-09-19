f63393a136d3e0833d97072fd7e667985e00e585
Vinny Diehl <vinny.diehl@gmail.com>
2025-09-18 22:10:08 -0700

    Allow click-to-move (in addition to drag-to-move)

--- app/chess/move.rb
+++ app/chess/move.rb
@@ -32,10 +32,19 @@ class ChessGame
         end
       end
 
+      x, y = mouse_board_pos
+
+      # Move a selected piece if one of the legal squares is clicked on
+      if @piece_selected &&
+         legal_moves(@piece_selected, @x_orig, @y_orig).include?([x, y])
+        move_piece(@piece_selected, x, y)
+        return
+      end
+
       # Otherwise, see if we're picking up a piece
 
       previous_selection = [@x_orig, @y_orig]
-      @x_orig, @y_orig = mouse_board_pos
+      @x_orig, @y_orig = x, y
       piece = @board[@x_orig][@y_orig]
 
       if !piece
@@ -89,138 +98,7 @@ class ChessGame
           return
         end
 
-        # There are multiple sounds that may be played. If this doesn't
-        # get set, the normal sound will be played later
-        sound = nil
-
-        # If we've captured a piece, save it
-        # En passant will be dealt with later
-        capture = @board[x][y]
-
-        # Resolve move
-        @board[x][y] = @piece_held
-        @piece_held = nil
-        piece_moved = @board[x][y]
-        @piece_selected = nil
-
-        # Castling
-        if piece_moved.type == :king
-          if (@x_orig - x).abs == 2
-            if x - @x_orig == 2
-              # Castled kingside
-              @board[7][y] = nil
-              @board[x - 1][y] = Piece.new(piece_moved.color, :rook)
-            else
-              # Castled queenside
-              @board[0][y] = nil
-              @board[x + 1][y] = Piece.new(piece_moved.color, :rook)
-            end
-
-            sound = :castle
-          end
-
-          # Moving the king means no more castling for that color
-          instance_variable_set("@#{piece_moved.color}_can_castle_kingside", false)
-          instance_variable_set("@#{piece_moved.color}_can_castle_queenside", false)
-        end
-        # If you move a rook, no more castling on that side
-        if piece_moved.type == :rook &&
-           [[0, 0], [0, 7], [7, 0], [7, 7]].include?([@x_orig, @y_orig])
-          color_affected = { 0 => :white, 7 => :black }[@y_orig]
-          side_affected = { 0 => :queenside, 7 => :kingside }[@x_orig]
-          instance_variable_set("@#{color_affected}_can_castle_#{side_affected}", false)
-        end
-
-        if piece_moved.type == :pawn
-          # En passant
-          ep_y = piece_moved.color == :white ? y - 1 : y + 1
-
-          # Set en passant target
-          if (y - @y_orig).abs == 2
-            @en_passant_target = [x, ep_y]
-          else
-            # Capture
-            if [x, y] == @en_passant_target
-              capture = @board[x][ep_y]
-              @board[x][ep_y] = nil
-            end
-
-            @en_passant_target = nil
-          end
-
-          # Pawn promotion
-          if (piece_moved.color == :white && y == 7) ||
-             (piece_moved.color == :black && y == 0)
-            @promotion = [x, y]
-          end
-        else
-          @en_passant_target = nil
-        end
-
-        if capture
-          @captures[@color_to_move] << capture
-          sound = :capture
-        end
-
-        # Switch turns
-        @color_to_move = OTHER_COLOR[@color_to_move]
-
-        update_notation(piece_moved, @piece_original_pos, x, y, capture)
-
-        # Increment move counters
-        @move_count += 1 if piece_moved.color == :black
-        @halfmove_total += 1
-        if piece_moved.type != :pawn && !capture
-          @halfmove_count += 1
-        else
-          @halfmove_count = 0
-        end
-
-        @last_move_squares = [@piece_original_pos, [x, y]]
-
-        @positions << position_entry
-        @current_position += 1
-
-        checkmate = checkmate?(@color_to_move)
-
-        # Draw situations
-        if !checkmate
-          # Fifty-move rule
-          draw = @halfmove_count >= 100
-
-          # Threefold repetition rule
-          #
-          # Requirements: same board position, same color to move, and same
-          #               legal moves for each piece for 3 times is a draw.
-          #
-          # The first 2 elements of the FEN and the legal moves recorded
-          # gives us this information.
-          draw ||= @positions.map { |p| [p[:fen].split(" ")[0..1], p[:legal_moves]] }
-                             .tally.any? { |_, count| count >= 3 }
-
-          if draw
-            @result = "½-½"
-            sound = :game_end
-          end
-        end
-
-        @piece_original_pos = nil
-
-        if (cm = checkmate) || stalemate?(@color_to_move)
-          @result = cm ? (@color_to_move == :black ? "1-0" : "0-1") : "½-½"
-          sound = :game_end
-        elsif in_check?(@color_to_move)
-          sound = :move_check
-        end
-
-        auto_scroll_notation
-
-        sound ||= :move_self
-
-        # Set sound if we're creating a new position
-        @positions[-1][:sound] = sound if on_last_position?
-
-        play_sound(sound)
+        move_piece(@piece_held, x, y)
       else
         # Tried to drag a piece off the board, put it back
         @board[@piece_original_pos.x][@piece_original_pos.y] = @piece_held
@@ -230,5 +108,145 @@ class ChessGame
         play_sound(:illegal)
       end
     end
+  end
+
+  def move_piece(piece, x, y)
+    # There are multiple sounds that may be played. If this doesn't
+    # get set, the normal sound will be played later
+    sound = nil
+
+    # If we've captured a piece, save it
+    # En passant will be dealt with later
+    capture = @board[x][y]
+
+    # Resolve move
+    @board[x][y] = piece
+    if @piece_held
+      @piece_held = nil
+    elsif @piece_selected
+      @piece_selected = nil
+      @board[@x_orig][@y_orig] = nil
+    end
+    piece = @board[x][y]
+    @piece_selected = nil
+
+    # Castling
+    if piece.type == :king
+      if (@x_orig - x).abs == 2
+        if x - @x_orig == 2
+          # Castled kingside
+          @board[7][y] = nil
+          @board[x - 1][y] = Piece.new(piece.color, :rook)
+        else
+          # Castled queenside
+          @board[0][y] = nil
+          @board[x + 1][y] = Piece.new(piece.color, :rook)
+        end
+
+        sound = :castle
+      end
+
+      # Moving the king means no more castling for that color
+      instance_variable_set("@#{piece.color}_can_castle_kingside", false)
+      instance_variable_set("@#{piece.color}_can_castle_queenside", false)
+    end
+    # If you move a rook, no more castling on that side
+    if piece.type == :rook &&
+       [[0, 0], [0, 7], [7, 0], [7, 7]].include?([@x_orig, @y_orig])
+      color_affected = { 0 => :white, 7 => :black }[@y_orig]
+      side_affected = { 0 => :queenside, 7 => :kingside }[@x_orig]
+      instance_variable_set("@#{color_affected}_can_castle_#{side_affected}", false)
+    end
+
+    if piece.type == :pawn
+      # En passant
+      ep_y = piece.color == :white ? y - 1 : y + 1
+
+      # Set en passant target
+      if (y - @y_orig).abs == 2
+        @en_passant_target = [x, ep_y]
+      else
+        # Capture
+        if [x, y] == @en_passant_target
+          capture = @board[x][ep_y]
+          @board[x][ep_y] = nil
+        end
+
+        @en_passant_target = nil
+      end
+
+      # Pawn promotion
+      if (piece.color == :white && y == 7) ||
+         (piece.color == :black && y == 0)
+        @promotion = [x, y]
+      end
+    else
+      @en_passant_target = nil
+    end
+
+    if capture
+      @captures[@color_to_move] << capture
+      sound = :capture
+    end
+
+    # Switch turns
+    @color_to_move = OTHER_COLOR[@color_to_move]
+
+    update_notation(piece, @piece_original_pos, x, y, capture)
+
+    # Increment move counters
+    @move_count += 1 if piece.color == :black
+    @halfmove_total += 1
+    if piece.type != :pawn && !capture
+      @halfmove_count += 1
+    else
+      @halfmove_count = 0
+    end
+
+    @last_move_squares = [@piece_original_pos, [x, y]]
+
+    @positions << position_entry
+    @current_position += 1
+
+    checkmate = checkmate?(@color_to_move)
+
+    # Draw situations
+    if !checkmate
+      # Fifty-move rule
+      draw = @halfmove_count >= 100
+
+      # Threefold repetition rule
+      #
+      # Requirements: same board position, same color to move, and same
+      #               legal moves for each piece for 3 times is a draw.
+      #
+      # The first 2 elements of the FEN and the legal moves recorded
+      # gives us this information.
+      draw ||= @positions.map { |p| [p[:fen].split(" ")[0..1], p[:legal_moves]] }
+                         .tally.any? { |_, count| count >= 3 }
+
+      if draw
+        @result = "½-½"
+        sound = :game_end
+      end
+    end
+
+    @piece_original_pos = nil
+
+    if (cm = checkmate) || stalemate?(@color_to_move)
+      @result = cm ? (@color_to_move == :black ? "1-0" : "0-1") : "½-½"
+      sound = :game_end
+    elsif in_check?(@color_to_move)
+      sound = :move_check
+    end
+
+    auto_scroll_notation
+
+    sound ||= :move_self
+
+    # Set sound if we're creating a new position
+    @positions[-1][:sound] = sound if on_last_position?
+
+    play_sound(sound)
   end
 end
